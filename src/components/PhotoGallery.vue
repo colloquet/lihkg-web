3b92f66a14661e94a704a9cdfb50fdd973c14d9c
Colloque Tsui <colloque.tsui@jwt.com>
2017-01-13 17:32:52 +0800

    Created photogallery component

---
+++ src/components/PhotoGallery.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="photo-mode-container">
+    <div class="uk-clearfix photo-mode-header">
+      <span class="uk-icon-image"></span> 圖片模式
+      <a class="uk-float-right photo-mode-close" @click.prevent="$emit('onClose')"><span class="uk-icon-remove"></span> 關閉</a>
+    </div>
+    <div class="photo-mode-body">
+      <div class="uk-container uk-container-center">
+        <p class="uk-text-center" v-if="noImages">此post冇圖片</p>
+        <div class="uk-grid uk-grid-small uk-grid-width-1-3 uk-grid-width-medium-1-4 uk-grid-width-large-1-5 uk-grid-width-xlarge-1-6" v-else>
+          <div v-for="image in images">
+            <a :href="image.url" data-uk-lightbox="{group:'photo-mode'}" data-lightbox-type="image">
+              <div class="image-container" :style="{backgroundImage: `url(${image.url})`}"></div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'modal',
+  props: ['no-images', 'images']
+}
+</script>
+
+<style lang="stylus">
+
+</style>
--- src/views/Thread.vue
+++ src/views/Thread.vue
@@ -90,24 +90,12 @@
     </div>
 
     <transition name="slide">
-      <div class="photo-mode-container" v-if="photoMode">
-        <div class="uk-clearfix photo-mode-header">
-          <span class="uk-icon-image"></span> 圖片模式
-          <a class="uk-float-right photo-mode-close" @click.prevent="photoMode = false"><span class="uk-icon-remove"></span> 關閉</a>
-        </div>
-        <div class="photo-mode-body">
-          <div class="uk-container uk-container-center">
-            <p class="uk-text-center" v-if="noImages">此post冇圖片</p>
-            <div class="uk-grid uk-grid-small uk-grid-width-1-3 uk-grid-width-medium-1-4 uk-grid-width-large-1-5 uk-grid-width-xlarge-1-6" v-else>
-              <div v-for="image in images">
-                <a :href="image.url" data-uk-lightbox="{group:'photo-mode'}" data-lightbox-type="image">
-                  <div class="image-container" :style="{backgroundImage: `url(${image.url})`}"></div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <photo-gallery
+        v-if="photoMode"
+        @onClose="photoMode = false"
+        :noImages="noImages"
+        :images="images"
+      />
     </transition>
   </div>
 </template>
@@ -116,13 +104,15 @@
 /* global $, UIkit, Image */
 import qrCode from 'qrcode-npm'
 import ThreadNavbar from '../components/ThreadNavbar'
+import PhotoGallery from '../components/PhotoGallery'
 import lihkg from '../api/lihkg'
 import helper from '../helper'
 
 export default {
   name: 'Thread',
   components: {
-    ThreadNavbar
+    ThreadNavbar,
+    PhotoGallery
   },
   data () {
     return {
